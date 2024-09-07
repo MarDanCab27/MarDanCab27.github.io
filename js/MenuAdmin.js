2c1ff10d0604adabff2fbaf641c1799c44bb44c3
juan del lantonia <juan.dellantonia.712@gmail.com>
2024-09-06 22:24:42 -0300

    Ajustes de menú para el administrador, mostrando de entrada las operaciones de carga a realizar.

---
+++ js/MenuAdmin.js
@@ -0,0 +1,8 @@
+document.getElementById('carga-user').addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = 'ambUsurios.html';
+})
+document.getElementById('carga-rutina').addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = 'actividadesMenu.html';
+})
